fe78fe5a9c073d9590b915fecde4e257eda2236d
Mina Karki <minakarki012@gmail.com>
2026-03-28 17:12:57 +0545

    JAVA Static Fields and Methods

---
+++ n_static_fields_method/a_static_variable.java
@@ -0,0 +1,11 @@
+package n_static_fields_method;
+
+public class a_static_variable {
+    static int roll = 1;
+    static String name = "Dibya";
+
+    public static void main(String[] args) {
+        System.out.println("Roll = " + roll);
+        System.out.println("Name = " + name);
+    }
+}
