30eb72b8fad96b40714994b5c0112a5c29b5db54
Mina Karki <minakarki012@gmail.com>
2026-03-31 21:24:29 +0545

    JAVA Final Keyword

---
+++ t_final_keyword/c_final_method.java
@@ -0,0 +1,23 @@
+package t_final_keyword;
+
+class Dad {
+    // overridden method
+    // final void cooking() {
+    void cooking() {
+        System.out.println("Dad is Cooking.");
+    }
+}
+
+class Daughter extends Dad {
+    @Override
+    // overriding method
+    void cooking() {
+        System.out.println("Daughter is Cooking.");
+    }
+}
+public class c_final_method {
+    public static void main(String[] args) {
+        Daughter keti = new Daughter();
+        keti.cooking();
+    }
+}
