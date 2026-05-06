b9496697cd4a13c43a86ca1993aff1bf752b6ff6
Mina Karki <minakarki012@gmail.com>
2026-05-06 21:30:42 +0545

    JAVA Final Keyword

--- t_final_keyword/b_final_class.java
+++ t_final_keyword/b_final_class.java
@@ -1,22 +1,14 @@
 package t_final_keyword;
 
 // final class SuperClass {
-class SuperClass {
-    void hi() {
-        System.out.println("Hi Students I'm super class.");
+final class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
     }
 }
-
-class SubClass extends SuperClass {
-    void hello() {
-        System.out.println("Hello Students I'm sub class.");
-    }
-}
-
 public class b_final_class {
     public static void main(String[] args) {
-        SubClass obj = new SubClass();
-        obj.hi();
-        obj.hello();
+        Animal obj = new Animal();
+        obj.sound();
     }
 }
